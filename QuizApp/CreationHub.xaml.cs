a9d5bb7bde8a6d6948af5325ca0b0a0b9bfc2263
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2022-01-20 17:46:33 +0100

    CreationHub Style

--- QuizApp/CreationHub.xaml.cs
+++ QuizApp/CreationHub.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace QuizApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CreationHub.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreationHub : Window
     {
-        public MainWindow()
+        public CreationHub()
         {
             InitializeComponent();
-        }
-
-        private void CreateBtn_Click(object sender, RoutedEventArgs e)
-        {
-            new CreationHub().Show();
-            Close();
         }
     }
 }
